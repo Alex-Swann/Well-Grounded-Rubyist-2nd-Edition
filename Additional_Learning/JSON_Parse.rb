c6fb42d283887c4b4962049cc3a4dc08c4ee7469
Lex-Castevet <alex_swann@hotmail.co.uk>
2016-04-21 14:47:05 +0100

    added JSON Parse example

---
+++ Additional_Learning/JSON_Parse.rb
@@ -0,0 +1,40 @@
+
+require 'json'
+
+ str = ' "specialties":["Corporate Housing","Temporary Housing","Furnished Apartment","Short Term Rentals"],
+
+"website":"http://www.demo.com",
+
+"universalName":"some-corporate-housing",
+
+"size":"51-200 employees",
+
+"description":"demo description",
+
+"industry":"Hospitality",
+
+"companyType":"Privately Held",
+
+"companyName":"some Corporate Housing",
+
+"includeSecondAd":true,
+
+"yearFounded":1995,
+
+"headquarters":
+
+  {"city":"Austin",
+   "zip":"78759",
+   "state":"Texas",
+   "street1":"9606 N. Mopac Expressway",
+   "country":"United States",
+   "street2":"Suite 500"
+  },
+
+"homeUrl":"https://www.some.com/company/cws-corporate-housing" '
+
+json_str= "{#{str}}"
+p hash = JSON.parse(json_str) #=> stuff
+
+
+
